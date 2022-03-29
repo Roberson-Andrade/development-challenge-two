11b7dd71e5c86aa64fdb1e657b6df4dd1e067811
Roberson Andrade <roberson_souza11@hotmail.com>
2022-03-29 11:09:01 -0300

    Config frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+function App() {
+  return (
+    <Container maxWidth='xl'>
+      <div>Hello World</div>
+    </Container>
+  );
+};
+
+export default App;
