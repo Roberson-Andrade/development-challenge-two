5d36fe4fba55608a075676ab65613cfa95acb4aa
Roberson Andrade <roberson_souza11@hotmail.com>
2022-03-31 09:29:02 -0300

    Add replace functionality

--- client/src/store/patientSlice.jsx
+++ client/src/store/patientSlice.jsx
@@ -1,57 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  patientItems: [
-    {
-      id: 'p1',
-      patientName: 'Sergio',
-      email: 'Sergio@hotmail.com',
-      address: 'Rua bonifacio vilela',
-      birthDay: '1988-05-02'
-    },
-    {
-      id: 'p2',
-      patientName: 'Larua',
-      email: 'Larua@hotmail.com',
-      address: 'Rua vilela',
-      birthDay: '1958-05-02'
-    },
-    {
-      id: 'p3',
-      patientName: 'Felipe',
-      email: 'Felipe@hotmail.com',
-      address: 'Rua bonifacio',
-      birthDay: '1988-08-10'
-    },
-    {
-      id: 'p4',
-      patientName: 'Wadler',
-      email: 'wadler@hotmail.com',
-      address: 'Rua santos',
-      birthDay: '1990-08-25'
-    },
-    {
-      id: 'p5',
-      patientName: 'Caren',
-      email: 'caren@hotmail.com',
-      address: 'Rua marechal',
-      birthDay: '2000-05-02'
-    },
-    {
-      id: 'p6',
-      patientName: 'Fabio',
-      email: 'fabio@hotmail.com',
-      address: 'Rua fabiano freitas',
-      birthDay: '1944-09-22'
-    },
-  ],
-  patientToEdit: {
-    id: '',
-    patientName: '',
-    email: '',
-    address: '',
-    birthDay: ''
-  }
+  patientItems: []
 }
 
 export const patientSlice = createSlice({
@@ -78,6 +28,9 @@ export const patientSlice = createSlice({
 
       state.patientItems[indexPatient] = action.payload
     },
+    replacePatients(state, action) {
+      state.patientItems = action.payload
+    }
   }
 
 })
